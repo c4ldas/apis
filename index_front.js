879dbf53af0c13c6fff1dd53317ba3d78cf781eb
Rodrigo Caldas <75918726+c4ldas@users.noreply.github.com>
2024-03-26 06:43:15 +0000

    Cleaning some code and fixing URLs

--- index_front.js
+++ index_front.js
@@ -2,29 +2,23 @@ const root = document.documentElement;
 const linkbox = document.querySelectorAll(".link-box");
 const mode = document.querySelector(".mode");
 const themeToggle = document.querySelector("#theme-toggle");
-const description = document.querySelectorAll(".description")
 
 function toggleTheme(obj) {
-  const isDark =
-    root.style.getPropertyValue("background-image") == "var(--bg-dark)";
+  const isDark = root.style.getPropertyValue("background-image") == "var(--bg-dark)";
   isDark ? lightMode() : darkMode();
 }
 
 function darkMode() {
   localStorage.setItem("theme", "dark"); // Store user preference
-  root.style.setProperty("background-image", "var(--bg-dark)");
-  root.style.setProperty("color", "var(--text-dark)");
+  root.style.setProperty("background-image", "var(--bg-dark)"); // Background color
+  root.style.setProperty("color", "var(--text-dark)"); // Font color
+  mode.style.setProperty("content", "url('../images/moon.svg')"); // Change image to moon
+  mode.style.setProperty("left", "70%"); // Change position of the moon image
   linkbox.forEach((box) => {
-    box.style.setProperty("color", "var(--text-dark)");
-    box.style.setProperty("background-color", "var(--box-bg-dark)");
-  });
-
-  description.forEach( (item) => {
-    item.style.setProperty("color", "var(--text-dark)");
-  });
-
-  mode.style.setProperty("content", "url('../images/moon.svg')");
-  mode.style.left = "70%";
+    box.style.setProperty("background-color", "var(--box-bg-dark)"); // Set link-box background color
+    const description = box.querySelector(".description"); // Get the description element inside link-box
+    description.style.setProperty("color", "var(--text-dark)"); // Set description text color
+  });  
   themeToggle.checked = true;
   return;
 }
@@ -33,33 +27,23 @@ function lightMode() {
   localStorage.setItem("theme", "light"); // Store user preference
   root.style.setProperty("background-image", "var(--bg-light)"); // Background color
   root.style.setProperty("color", "var(--text-light)"); // Font color
-
-  description.forEach( (item) => {
-    item.style.setProperty("color", "var(--text-light)");
-  });
-
+  mode.style.setProperty("content", "url('../images/sun.svg')"); // Change image to sun
+  mode.style.setProperty("left", "25%"); // Change position of the sun image
   linkbox.forEach((box) => {
-    box.style.setProperty("color", "var(--text-light)");
-    box.style.setProperty("background-color", "var(--box-bg-light)");
-  });
-
-  mode.style.setProperty("content", "url('../images/sun.svg')");
-  mode.style.left = "25%";
+    box.style.setProperty("background-color", "var(--box-bg-light)"); // Set link-box background color
+    const description = box.querySelector(".description"); // Get the description element inside link-box
+    description.style.setProperty("color", "var(--text-light)"); // Set description text color
+  });  
   themeToggle.checked = false;
   return;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  // Check the current user preference for theme color
-  const savedTheme = localStorage.getItem("theme");
-  console.log(savedTheme);
+document.addEventListener("DOMContentLoaded", () => {  
+  const savedTheme = localStorage.getItem("theme"); // Check the current user preference for theme color
 
-  // In case customer didn't have set a preference, check the system-wide preference
-  // Defaults to light mode.
+  // Check the system-wide preference, if no preference set previously. Defaults to light mode.
   if (!savedTheme) {
-    const prefersDarkMode = window.matchMedia(
-      "(prefers-color-scheme: dark)",
-    ).matches;
+    const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
     prefersDarkMode ? darkMode() : lightMode();
     return;
   }
