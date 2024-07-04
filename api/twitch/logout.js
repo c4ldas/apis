0a02eb20f5ef7e0aa9a66074be2e9dabb0a1271c
Rodrigo Caldas <75918726+c4ldas@users.noreply.github.com>
2024-07-04 14:01:41 +0000

    Removed comments

--- api/twitch/logout.js
+++ api/twitch/logout.js
@@ -17,38 +17,11 @@ router.post('/', async (req, res) => {
     const removed = await db.delete(req.body.user_key);
     
     res.status(200).json({ status: "success", message: "Integration removed successfully" });
-  
-  // if(!removed.ok) {
-  //   res.status(200).json({ status: "failed", message: "There was an error while removing the integration, try again later"})
-  // } else {
-  //   res.status(200).json({ status: "success", message: "Integration removed successfully" });
-  // }
     
   } catch (error) {
     console.log("Logout error:", error);
     res.status(500).json({ status: "failed", message: "There was an error while removing the integration. Try again later"})
   }
-
-   // const removed = await db.delete(req.body.user_key);
-   // console.log(removed);
-   // 
-   // if(!removed.ok) {
-   //   res.status(200).json({ status: "failed", message: "There was an error while removing the integration, try again later"})
-   // } else {
-   //   res.status(200).json({ status: "success", message: "Integration removed successfully" });
-   // }
-
 })
 
 module.exports = router;
-
-/*
-curl -X POST "https://id.twitch.tv/oauth2/revoke?client_id=1mhvnqfp2xtswwqz1p8ol3doqm4t26&token=iwpth8f683wpr63bn2zjcz5nubu7prdcn9ownikg69gmmv60m0"
-
-
-curl -X POST 'https://id.twitch.tv/oauth2/revoke' \
--H 'Content-Type: application/x-www-form-urlencoded' \
--d 'client_id=<client id goes here>&token=<access token goes here>'
-
-*/
-
