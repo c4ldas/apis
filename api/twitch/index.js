bb181d152da2bb8c174ed54412b460f57af74f93
Rodrigo Caldas <75918726+c4ldas@users.noreply.github.com>
2024-07-04 13:48:51 +0000

    Moved Twitch routes from `/index.js` to `/api/twitch/index.js` file

--- api/twitch/index.js
+++ api/twitch/index.js
@@ -1,12 +1,19 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const code = require('./callback.js').code
+router.use("/login", require("./login"));
+router.use("/callback", require("./callback"));
+router.use("/auth", require("./auth"));
+router.use("/logout", require("./logout"));
+router.use("/prediction", require("./prediction"));
 
-router.use(express.static(__dirname))
 
 router.get('/', async (req, res) => {
+  console.log("Sei lá vei");
   res.sendFile(__dirname + '/index.html');
-})
+});
 
 module.exports = router;
+
+
+
