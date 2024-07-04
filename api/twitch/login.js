cb828316cbcc047eabab8344dd7a127744bb056a
Rodrigo Caldas <75918726+c4ldas@users.noreply.github.com>
2024-07-04 13:50:51 +0000

    Added dev environment for redirect uri

--- api/twitch/login.js
+++ api/twitch/login.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+const TWITCH_REDIRECT_URI = process.env.REPLIT_DEV_DOMAIN 
+  ? `https://${process.env.REPLIT_DEV_DOMAIN}/api/twitch/callback` 
+  : `${process.env.TWITCH_CLIENT_ID}`;
+
 router.get('/', async (req, res) => {
   res.redirect('https://id.twitch.tv/oauth2/authorize?' +
     new URLSearchParams({
       force_verify: false,
       client_id: process.env.TWITCH_CLIENT_ID,
-      redirect_uri: process.env.TWITCH_REDIRECT_URI,
+      redirect_uri: TWITCH_REDIRECT_URI,
       response_type: 'code',
       scope: 'channel:read:predictions channel:manage:predictions',
       state: ''
