1cb23bd048391ec512e9f1e4e19b6b6b727e23e1
Rodrigo Caldas <75918726+c4ldas@users.noreply.github.com>
2024-07-05 16:05:55 +0000

    Moved the Spotify routes to /api/spotify/index.js

--- api/spotify/index.js
+++ api/spotify/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router()
 
+router.use("/login", require("./login"));
+router.use("/callback", require("./callback"));
+router.use("/musica", require("./musica"));
 
-router.use(express.static(__dirname))
-
-router.get('/', async (req, res) => { 
-  res.sendFile(__dirname + '/index.html');
-  
-})
+// router.get('/', async (req, res) => { 
+//   console.log("This route is not needed if there is an index.html in the folder")
+//   res.sendFile(__dirname + '/index.html');
+// }
 
 module.exports = router;
