13fd282b8d3b10b700f74371c9a10c732b93fbf2
Rodrigo Caldas <75918726+c4ldas@users.noreply.github.com>
2024-09-16 19:55:10 +0000

    Moved routes to index.js

--- api/steam/index.js
+++ api/steam/index.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
+router.use("/game", require("./game"));
 
-router.use(express.static(__dirname))
-
+// When accessing twitch home, it doesn't access this function,
+// By default, express will look for index.html 
+// In case index.html is not found, it checks the route "/"
 router.get('/', async (req, res) => {
   res.sendFile(__dirname + '/index.html');
 })
 
 module.exports = router;
+
